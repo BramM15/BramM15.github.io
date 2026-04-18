a0c0976151de512a7e70ebd13bc6bf7f767fba54
BramM15 <bramfabian2006@gmail.com>
2026-04-18 13:19:25 +0200

    setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className='text-2xl'>Site under construction</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
